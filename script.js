f229bae4573e3c7e06d5aae8c3c9e75dff3ddd91
alamreshad <144240453+alamreshad@users.noreply.github.com>
2023-12-04 09:14:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Animations */
+let type = new Typed('#next-text', {
+    strings: ['UMich Student.', 'UX Designer.', 'Photographer.'],
+    typespeed: 50,
+    backspeed: 50,
+    backDelay: 1500,
+    loop: true,
+});
